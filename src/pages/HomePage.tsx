edfaff9eb03fdf30c140cf4957946b3330ed7641
vitaliikravchyk <vitaly.kravchyk@gmail.com>
2025-09-09 18:08:41 +0300

    Fixed root path

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,5 @@
-import React, { useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import React from 'react';
 
 export const HomePage: React.FC = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  useEffect(() => {
-    console.log(searchParams);
-    const params = new URLSearchParams(searchParams);
-
-    for (const [key, value] of params) {
-      console.log('key', key, 'value', value);
-    }
-  }, [searchParams]);
-
   return <h1 className="title">Home page</h1>;
 };
--- src/router/AppModernRouter.tsx
+++ src/router/AppModernRouter.tsx
@@ -7,7 +7,7 @@ import { TabContentPage } from '../pages/TabContentPage';
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: '',
     element: <App />,
     children: [
       {
